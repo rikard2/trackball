62ec55448210cd6f78ec7d46469a060b63ade16a
Rikard Javelind <rikard.javelind@trustly.com>
2017-06-05 23:18:50 +0200

    omg

--- fps_demo.py
+++ fps_demo.py
@@ -28,6 +28,8 @@ while fps._numFrames < args["num_frames"]:
 	# grab the frame from the stream and resize it to have a maximum
 	# width of 400 pixels
 	(grabbed, frame) = stream.read()
+	frame = cv2.GaussianBlur(frame, (5, 5), 0)
+	frame = cv2.Canny(frame, 100, 100)
 	#frame = imutils.resize(frame, width=400)
 
 	# check to see if the frame should be displayed to our screen
@@ -50,17 +52,17 @@ cv2.destroyAllWindows()
 # created a *threaded *video stream, allow the camera senor to warmup,
 # and start the FPS counter
 print("[INFO] sampling THREADED frames from webcam...")
-vs = WebcamVideoStream(src=0).start()
+stream = cv2.VideoCapture('rally2.mpg')
 fps = FPS().start()
 
 # loop over some frames...this time using the threaded stream
 while fps._numFrames < args["num_frames"]:
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
-	frame = vs.read()
+	(grabbed, frame) = stream.read()
 	#frame = imutils.resize(frame, width=400)
-        frame = cv2.GaussianBlur(frame, (5, 5), 0)
-        frame = cv2.Canny(frame, 100, 100)
+	frame = cv2.GaussianBlur(frame, (5, 5), 0)
+	frame = cv2.Canny(frame, 100, 100)
 	# check to see if the frame should be displayed to our screen
 	if args["display"] > 0:
 		cv2.imshow("Frame", frame)
